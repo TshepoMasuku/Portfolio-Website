3a321400dd0ae8d7624e97069e866d69152c938e
TshepoMasuku <tshepomasuku16@gmail.com>
2024-04-24 11:36:12 +0200

    Contact me Form Validation done!

--- public/contact.js
+++ public/contact.js
@@ -1,31 +1,97 @@
-// should reset Contact me Form after submitting email. 
-// window.onbeforeunload = () => {
-//   for(const form of document.getElementsByTagName('form')) {
-//     form.reset();
-//   }
-// }
-
-
 // Contact me Form - Validation.
-// must be able to remove the span-tag hidden class for an exact input-id in the form. 
-// Name(s)*
-// Surname*
-// Email Address*
-// Subject*
-// Your Message/Query*
-const nameInput = document.getElementById("names");
-const surnameInput = document.getElementById("surname");
-const emailInput = document.getElementById("email");
-const subjectInput = document.getElementById("subject");
-const messageInput = document.getElementById("message");
-const submitBtn = document.getElementsByClassName("emailForm__submitBtn");
+function validateForm() {
+  // Name(s)*, Surname*, Email Address*, Subject* & Your Message/Query*.
+	const nameInput = document.getElementById("names");
+	const surnameInput = document.getElementById("surname");
+	const emailInput = document.getElementById("email");
+	const subjectInput = document.getElementById("subject");
+	const messageInput = document.getElementById("message");
+  
+  // Boolean Values to check if a specific input is valid.
+	let isNameInputValid = false;
+	let isSurnameInputValid = false;
+	let isEmailInputValid = false;
+	let isSubjectInputValid = false;
+	let isMessageInputValid = false;
+  let isFormValidated = false;
+  
+  // must be able to remove/add the html tag's hidden class for a specific invalid input in the form. 
+	if (nameInput.value.trim() === "") {
+    const errorSpan = document.getElementById("namesSpan");
+    errorSpan.innerText = "name(s) input invalid";
+    errorSpan.classList.remove("hidden");
+	} else {
+    const errorSpan = document.getElementById("namesSpan");
+    errorSpan.classList.add("hidden");
+    isNameInputValid = true;
+  }
 
-// submitBtn.addEventListener(("click") => {
-//   const requiredInput = document.getElementsByTagName("span");
-//   requiredInput.classList.remove("hidden");
-// });
+  if (surnameInput.value.trim() === "") {
+    const errorSpan = document.getElementById("surnameSpan");
+    errorSpan.innerText = "surname input invalid";
+    errorSpan.classList.remove("hidden");
+	} else {
+    const errorSpan = document.getElementById("surnameSpan");
+    errorSpan.classList.add("hidden");
+    isSurnameInputValid = true;
+  }
 
-function handleSubmit(event) {
-	event.preventDefault();
-	console.log(event);
+  if (emailInput.value.trim() === "") {
+    const errorSpan = document.getElementById("emailSpan");
+    errorSpan.innerText = "email input invalid";
+    errorSpan.classList.remove("hidden");
+	} else {
+    const errorSpan = document.getElementById("emailSpan");
+    errorSpan.classList.add("hidden");
+    isEmailInputValid = true;
+  }
+
+  if (subjectInput.value.trim() === "") {
+    const errorSpan = document.getElementById("subjectSpan");
+    errorSpan.innerText = "subject input invalid";
+    errorSpan.classList.remove("hidden");
+	} else {
+    const errorSpan = document.getElementById("subjectSpan");
+    errorSpan.classList.add("hidden");
+    isSubjectInputValid = true;
+  }
+
+  if (messageInput.value.trim() === "") {
+    const errorSpan = document.getElementById("messageSpan");
+    errorSpan.innerText = "message input invalid";
+    errorSpan.classList.remove("hidden");
+	} else {
+    const errorSpan = document.getElementById("messageSpan");
+    errorSpan.classList.add("hidden");
+    isMessageInputValid = true;
+  }
+
+  isFormValidated = isNameInputValid && 
+    isSurnameInputValid && isEmailInputValid && 
+    isSubjectInputValid && isMessageInputValid;
+  return isFormValidated;
+}
+
+function handleSubmit(event) { 
+  const isFormValidated = validateForm();
+  // console.log('HandleSubmit() -- isFormValidated :>> ', isFormValidated); 
+  if (isFormValidated === false) {
+    event.preventDefault(); 
+  };
+}
+
+function handleClear() { 
+  // Get the HTML elements from the DOM using their id's.
+  const namesInput = document.getElementById("names");
+  const surnameInput = document.getElementById("surname");
+  const emailInput = document.getElementById("email");
+	const subjectInput = document.getElementById("subject");
+	const messageInput = document.getElementById("message");
+  
+  // Change the DOM elements values into empty strings.
+  namesInput.value = "";
+  surnameInput.value = "";
+  emailInput.value = "";
+  subjectInput.value = "";
+  messageInput.value = "";
 }
